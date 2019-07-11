0de81094f9a54d6b37d62cf58f48778a5a67bdf6
lvhongbin <lvhongbin@appledeMacBook-Pro.local>
2019-07-11 16:15:16 +0800

    "update index.js"

--- bin/index.js
+++ bin/index.js
@@ -6,7 +6,7 @@ const chalk = require('chalk');
 const ora = require('ora');
 
 program
-  .version('1.0.0', '-v, --version')
+  .version('1.0.1', '-v, --version')
   .option('-i, init [name]', '初始化lhbcli-self项目')
   .parse(process.argv);
 
@@ -15,9 +15,11 @@ if (program.init) {
   download('hblvsjtu/lhbcli-self', program.init, function (err) {
     if(!err){
       // 可以输出一些项目成功的信息
-      console.info(chalk.blueBright('下载成功'));
+      //console.info(chalk.blueBright('下载成功'));
+      spinner.succeed(chalk.greenBright('下载成功'));
     }else{
       // 可以输出一些项目失败的信息
+      spinner.fail(chalk.redBright('下载失败'));
     }
   })
 }
